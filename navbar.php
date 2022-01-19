02717e2401605896058f5735976eed7f75ec5ee4
arth347 <arthgamer13@gmail.com>
2022-01-19 14:05:01 -0300

    atualização tamanho da tela do celular A10

--- navbar.php
+++ navbar.php
@@ -32,7 +32,7 @@
                   <ul class="ul_dos_links_da_navbar_mobile">
                      <li class="decoracao-nav-mobile"><a href="index.php">Inicio</a></li>
                      <li><a href="index.php#filmes">Filmes</a></li>
-                     <li class="decoracao-nav-mobile"><a href="memes-e-wallpapers.php">Memes</a></li>
+                     <li class="decoracao-nav-mobile"><a href="memes-e-wallpapers.php#memes">Memes</a></li>
                      <li><a href="memes-e-wallpapers.php#wallpapers">Wallpapers</a></li>
                      <li class="decoracao-nav-mobile"><a href="quiz-do-shrek.php">Quiz do Shrek</a></li>
                  </ul>
